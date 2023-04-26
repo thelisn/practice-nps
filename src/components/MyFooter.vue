2ac3431d443a7a6b660b062ac7b932760491e0f6
thelisn <lisn.public@daum.net>
2023-04-26 14:23:25 +0900

    footer, 연혁 완성

--- src/components/MyFooter.vue
+++ src/components/MyFooter.vue
@@ -3,23 +3,29 @@
     <hr>
     <div class="center">
       <div class="center_box">
-      <img src="@/assets/images/f_center_icon.svg" alt="국민연금 고객센터">
+        <img src="@/assets/images/f_center_icon.svg" alt="국민연금 고객센터">
       </div>
-      <p><span>국민연금 고객센터 &#8594;</span> 국번없이 <span>1</span><span>3</span><span>55</span>(유료) 해외 +82-63-713-6900 / 평일 09:00 ~ 18:00</p>
+      <p><span>국민연금 고객센터 &#8594;</span> 국번없이 <span>1</span><span>3</span><span>55</span>(유료) 해외 +82-63-713-6900 / <span>평일 09:00 ~ 18:00</span></p>
     </div>
     <a href="#" class="video" onclick="return false">
-      <div class="video_box"><img src="@/assets/images/f_video_icon.svg" alt="영상수어상담"></div>
+      <div class="video_box">
+        <img src="@/assets/images/f_video_icon.svg" alt="영상수어상담">
+      </div>
       <p>영상수어상담 &#8594;</p>
     </a>
     <div class="bar">
       <div class="txt">
-      <a href="#" v-for="txt in txt" :key="txt"><p>{{ txt }}</p></a>
+        <a href="#" v-for="txt in txt" :key="txt">
+          <p>{{ txt }}</p>
+        </a>
       </div>
 
       <div class="sns">
-      <a href="#"><div class="sns_box" v-for="img in img" :key="img">
-        <img :src="img" alt="sns아이콘">
-      </div></a>
+        <div class="sns_box" v-for="img in img" :key="img">
+          <a href="#">
+            <img :src="img" alt="sns아이콘">
+          </a>
+        </div>
       </div>
     </div>
   </div>
@@ -48,41 +54,56 @@ export default {
   position: relative;
   width: 1100px;
   margin: 0 auto;
-  background: #ccc;
-  margin-top: 60px;
-  margin-bottom: 100px;
+  margin-top: 80px;
+  margin-bottom: 30px;
+  // background: #ccc;
 
   hr {
+    height:1px;
     margin-bottom: 60px;
+    border: 0;
+    background: #ddd;
   }
 
   .center {
 
     .center_box {
+      display: inline-block;
       width: 30px;
       height: 30px;
       border-radius: 10px;
-      background-color: green;
+      background-color: #008000;
+
+      img {
+        width: 15px;
+        margin: 0 auto;
+        transform: translateY(7px);
+        // background-color: pink;
+      }
     }
 
     p {
-      font-family: 'NanumSquareBold';
+      display: inline-block;
+      margin-left: 10px;
       font-size: 2.2em;
+      font-family: 'NanumSquareBold';
+      transform: translateY(-8px);
+      // background-color: pink;
 
       span:nth-child(1) {
-        color: green;
+        color: #008000;
       }
 
       span:nth-child(2) {
-        color: orangered;
+        color: #d13800;
       }
 
       span:nth-child(3) {
-        color: green;
+        color: #008000;
       }
 
       span:nth-child(4) {
-        color: blue;
+        color: #0000bb;
       }
     }
   }
@@ -92,10 +113,28 @@ export default {
     top: 60px;
     right: 0;
 
+    .video_box {
+      display: inline-block;
+      width: 30px;
+      height: 30px;
+      border-radius: 10px;
+      background: #0000bb;
+
+      img {
+        width: 15px;
+        margin: 0 auto;
+        transform: translateY(7px);
+      }
+    }
+
     p {
+      display: inline-block;
+      margin-left: 10px;
+      color: #0000bb;
       font-size: 2.2em;
-      color: blue;
       font-family: 'NanumSquareBold';
+      transform: translateY(-8px);
+      // background-color: pink;
     }
   }
 
@@ -104,25 +143,26 @@ export default {
     width: 100%;
     height: 70px;
     margin-top: 30px;
-    background: #eee;
     border-radius: 15px;
+    background: #eee;
 
     a {
       display: inline-block;
     }
 
     .txt {
-      background: yellow;
       width:750px;
       height: 100%;
-      margin-left: 30px;
-
+      margin-left: 20px;
+      // background: yellow;
+      
       a {
         margin-right: 26px;
-
+        
         &:nth-child(1) {
+          
           p {
-            color: blue;
+            color: #0000bb;
             font-family: 'NanumSquareBold';
           }
         }
@@ -145,11 +185,161 @@ export default {
       right: 20px;
       width: 300px;
       height: 100%;
-      background-color: pink;
+      // background-color: pink;
 
-      a {
-        line-height: 75px;
-        margin-right: 10px;
+        .sns_box {
+          display: inline-block;
+          width: 40px;
+          height: 40px;
+          margin-top: 12px;
+          margin-right: 10px;
+          border: 1px solid #ccc;
+          border-radius: 20px;
+          background: #fff;
+
+          &:last-child {
+            margin-right: 0px;   
+          }
+
+          &:hover img {
+            transform: scale(1.1);
+          }
+          
+          a {
+          width: 100%;
+          // line-height: 75px;
+          // margin-right: 10px;
+          // background-color: pink;
+          
+          img {
+            width:100%;
+            transition: .2s;
+          }
+        }
+      }
+    }
+  }
+}
+
+@media screen and (max-width: 1200px) {
+  .footer {
+    width: 90%;
+
+    .center {
+
+      .center_box {
+        width: 26px;
+        height: 26px;
+        border-radius: 8px;
+
+        img {
+          width: 13px;
+        }
+      }
+
+      p {
+        margin-left: 8px;
+        font-size: 1.8em;
+      }
+    }
+
+    .video {
+
+      .video_box {
+        width: 26px;
+        height: 26px;
+        border-radius: 8px;
+
+        img {
+          width: 13px;
+        }
+      }
+
+      p {
+        margin-left: 8px;
+        font-size: 1.8em;
+      }
+    }
+
+    .bar {
+
+      .txt {
+
+        a {
+
+          &:nth-child(3),
+          &:nth-child(6),
+          &:nth-child(7) {
+            display: none;
+          }
+
+          p {
+            font-size: 1.3em;
+          }
+        }
+      }
+    }
+  }
+}
+
+@media screen and (max-width: 950px) {
+  .footer {
+
+    hr {
+      margin-bottom: 40px;
+    }
+
+    .center {
+
+      p {
+        margin-left: 20px;
+        font-size: 1.6em;
+        line-height: 20px;
+        transform: translateY(28px);
+        // background-color: pink;
+
+        span:nth-child(1) {
+          display: block;
+        }
+
+        span:nth-child(5) {
+          display: block;
+        }
+      }
+    }
+
+    .video {
+      top: 160px;
+      left: 0;
+
+      p {
+        margin-left: 20px;
+        font-size: 1.6em;
+      }
+    }
+
+    .bar {
+      height: 120px;
+      margin-top: 120px;
+
+      .txt {
+        width: 100%;
+        margin-left: 0px;
+        text-align: center;
+        // background-color: pink;
+
+        a {
+
+          p {
+            font-size: 1.6em;
+          }
+        }
+      }
+
+      .sns {
+        top: 50px;
+        left: 50%;
+        transform: translateX(-50%);
       }
     }
   }
--- src/components/MyHistory.vue
+++ src/components/MyHistory.vue
@@ -39,41 +39,41 @@ export default {
   data() {
     return {
       first: [
-        {date: '1986.12.31', desc: '국민연금법 공포'},
-        {date: '1987.09.18', desc: '국민연금관리공단 설립'},
-        {date: '1988.01.01', desc: '국민연금제도 실시(10인 이상 사업장)'},
-        {date: '1992.01.01', desc: '당연적용 사업장 확대(5인 이상 사업장)'},
-        {date: '1993.01.01', desc: '특례노령연금 지급 개시'},
-        {date: '1995.04.01', desc: '국민연금연구센터 설치'},
-        {date: '1995.07.01', desc: '농어촌지역 주민 확대 적용'},
-        {date: '1995.08.04', desc: '사업장 종사 외국인 당연 적용'}
+        { date: '1986.12.31', desc: '국민연금법 공포' },
+        { date: '1987.09.18', desc: '국민연금관리공단 설립' },
+        { date: '1988.01.01', desc: '국민연금제도 실시(10인 이상 사업장)' },
+        { date: '1992.01.01', desc: '당연적용 사업장 확대(5인 이상 사업장)' },
+        { date: '1993.01.01', desc: '특례노령연금 지급 개시' },
+        { date: '1995.04.01', desc: '국민연금연구센터 설치' },
+        { date: '1995.07.01', desc: '농어촌지역 주민 확대 적용' },
+        { date: '1995.08.04', desc: '사업장 종사 외국인 당연 적용' }
       ],
       second: [
-        {date: '1999.01.01', desc: '조기노령연금 지급 개시'},
-        {date: '1999.04.01', desc: '도시지역 확대 적용(전국민 연금실시)'},
-        {date: '1999.11.05', desc: '기금운용본부 설치'},
-        {date: '2000.07.01', desc: '농어촌지역 특례노령연금 지급 개시'},
-        {date: '2001.11.13', desc: '텔레서비스시스템 전국 확대 운영'},
-        {date: '2002.11.04', desc: '4대사회보험정보연계 서비스 시행'},
-        {date: '2003.01.01', desc: '감액노령연금 및 재직자노령연금 지급 개시'},
-        {date: '2003.07.01', desc: '당연적용 사업장 확대 적용(1단계)<br>- 5인미만 사업장 중 법인/전문직종 사업장<br>- 5인이상 사업장의 비정규직 근로자'},
-        {date: '2004.04.01', desc: '도시지역 특례노령연금 지급 개시'},
-        {date: '2004.07.01', desc: '당연적용 사업장 확대 적용(2단계)<br>(국민건강보험/고용보험가입 사업장)'},
+        { date: '1999.01.01', desc: '조기노령연금 지급 개시' },
+        { date: '1999.04.01', desc: '도시지역 확대 적용(전국민 연금실시)' },
+        { date: '1999.11.05', desc: '기금운용본부 설치' },
+        { date: '2000.07.01', desc: '농어촌지역 특례노령연금 지급 개시' },
+        { date: '2001.11.13', desc: '텔레서비스시스템 전국 확대 운영' },
+        { date: '2002.11.04', desc: '4대사회보험정보연계 서비스 시행' },
+        { date: '2003.01.01', desc: '감액노령연금 및 재직자노령연금 지급 개시' },
+        { date: '2003.07.01', desc: '당연적용 사업장 확대 적용(1단계)<br>- 5인미만 사업장 중 법인/전문직종 사업장<br>- 5인이상 사업장의 비정규직 근로자' },
+        { date: '2004.04.01', desc: '도시지역 특례노령연금 지급 개시' },
+        { date: '2004.07.01', desc: '당연적용 사업장 확대 적용(2단계)<br>(국민건강보험/고용보험가입 사업장)' },
       ],
       third: [
-        {date: '2006.01.01', desc: '당연적용사업장 확대적용(3단계)<br>(근로자 1인이상 사업장)'},
-        {date: '2006.09.18', desc: '국민연금 新 CI발표'},
-        {date: '2007.07.23', desc: '"국민연금공단"으로 명칭 변경'},
-        {date: '2008.01.01', desc: '완전노령연금 지급 개시'},
-        {date: '2009.05.01', desc: '노후설계서비스(CSA) 사업 시행'},
-        {date: '2009.05.14', desc: '외국인 전담조직 국제업무센터 설립 및 개설'},
-        {date: '2009.08.07', desc: '국민연금과 4개 직역연금 가입기간 연계사업시행'},
-        {date: '2011.01.01', desc: '사회보험 징수통합에 따른 징수업무 이관'},
-        {date: '2011.04.01', desc: '장애인복지법 상 장애 전(全)등급 심사 개시'},
-        {date: '2011.10.05', desc: '장애인 활동지원 사업 시행'},
-        {date: '2012.12.01', desc: '기초수급자 근로능력평가 사업 수행'},
-        {date: '2014.07.01', desc: '기초연금 사업 수행'},
-        {date: '2015.12.23', desc: '전 국민 대상 노후준비서비스 시행'}
+        { date: '2006.01.01', desc: '당연적용사업장 확대적용(3단계)<br>(근로자 1인이상 사업장)' },
+        { date: '2006.09.18', desc: '국민연금 新 CI발표' },
+        { date: '2007.07.23', desc: '"국민연금공단"으로 명칭 변경' },
+        { date: '2008.01.01', desc: '완전노령연금 지급 개시' },
+        { date: '2009.05.01', desc: '노후설계서비스(CSA) 사업 시행' },
+        { date: '2009.05.14', desc: '외국인 전담조직 국제업무센터 설립 및 개설' },
+        { date: '2009.08.07', desc: '국민연금과 4개 직역연금 가입기간 연계사업시행' },
+        { date: '2011.01.01', desc: '사회보험 징수통합에 따른 징수업무 이관' },
+        { date: '2011.04.01', desc: '장애인복지법 상 장애 전(全)등급 심사 개시' },
+        { date: '2011.10.05', desc: '장애인 활동지원 사업 시행' },
+        { date: '2012.12.01', desc: '기초수급자 근로능력평가 사업 수행' },
+        { date: '2014.07.01', desc: '기초연금 사업 수행' },
+        { date: '2015.12.23', desc: '전 국민 대상 노후준비서비스 시행' }
       ]
     }
   }
@@ -88,91 +88,89 @@ export default {
   // background-color: yellow;
 
   h2 {
+    display: inline-block;
     height: 500px;
     font-size: 4.2em;
     font-family: 'NanumSquareExtraBold';
-    // font-weight: bold;
     // background-color: yellow;
-    display: inline-block;
+    // font-weight: bold;
 
     &::after {
       display: block;
       position: absolute;
       top: 10px;
       left: 250px;
-      border-radius: 5px;
       width: 10px;
       height: 10px;
-      content: '';
+      border-radius: 5px;
       background: #92a8d1;
+      content: '';
     }
   }
 
-    .y_1986,
-    .y_1995,
-    .y_1996,
-    .y_2005,
-    .y_2006
-     {
-      position: absolute;
-      right: 0;
-      // display: inline-block;
-      font-family: 'NanumSquareExtraBold';
-      font-size: 10em;
-      opacity: .1;
-      // background-color: yellow;
-    }
+  .y_1986,
+  .y_1995,
+  .y_1996,
+  .y_2005,
+  .y_2006 {
+    position: absolute;
+    right: 0;
+    font-size: 10em;
+    font-family: 'NanumSquareExtraBold';
+    opacity: .1;
+    // display: inline-block;
+    // background-color: yellow;
+  }
 
-    .y_1986,
-    .y_1996,
-    .y_2006 {
-      top: 0;
-    }
+  .y_1986,
+  .y_1996,
+  .y_2006 {
+    top: 0;
+  }
 
   .first {
-    width: 100%;
     position: relative;
+    width: 100%;
     height: 500px;
-    // background: pink;
     margin-bottom: 30px;
+    // background: pink;
 
     .first_list {
-      width: 800px;
-      // background: coral;
       position: relative;
       top: -495px;
       left: 300px;
+      width: 800px;
       // position: absolute;
+      // background: coral;
       // height: 500px;
 
       &::before {
         content: '';
         display: block;
+        position: absolute;
         top: 10px;
         right: 845px;
         z-index: -9;
-        position: absolute;
         width: 1px;
         height: 1600px;
+        background: linear-gradient(#f7cac9, #92a8d1);
         // height: 300px;
         // background: #ccc;
-        background: linear-gradient(#f7cac9, #92a8d1);
       }
 
       .first_date {
-        width: 100px;
-        // background-color: salmon;
-        font-size: 2.2em;
         display: inline-block;
+        width: 100px;
         margin-bottom: 30px;
         margin-right: 40px;
-
+        font-size: 2.2em;
+        // background-color: salmon;
       }
 
       .first_desc {
+        display: inline-block;
         font-size: 2em;
         line-height: 30px;
-        display: inline-block;
         // background: palevioletred;
       }
     }
@@ -183,19 +181,20 @@ export default {
   }
 
   .second {
-    width: 100%;
     position: relative;
+    width: 100%;
     height: 700px;
     margin-bottom: 30px;
     // background-color: pink;
 
     .second_list {
-      width: 800px;
       position: relative;
       top: -495px;
       left: 300px;
+      width: 800px;
 
       &:nth-child(9) {
+
         .second_date {
           transform: translateY(-60px);
         }
@@ -208,18 +207,18 @@ export default {
       }
 
       .second_date {
-        width: 100px;
-        font-size: 2.2em;
         display: inline-block;
-        margin-bottom: 30px;
+        width: 100px;
         margin-right: 40px;
+        margin-bottom: 30px;
+        font-size: 2.2em;
         // background-color: salmon;
       }
 
       .second_desc {
+        display: inline-block;
         font-size: 2em;
         line-height: 30px;
-        display: inline-block;
         // background-color: palegoldenrod;
       }
     }
@@ -230,35 +229,36 @@ export default {
   }
 
   .third {
+    position: relative;
     width: 100%;
     height: 500px;
-    position: relative;
-    
+
     .third_list {
-      // background-color: pink;
-      width: 800px;
       position: relative;
       top: -495px;
       left: 300px;
+      width: 800px;
+      // background-color: pink;
 
       &:nth-child(2) {
+
         .third_date {
           transform: translateY(-30px);
         }
       }
 
       .third_date {
-        width: 100px;
-        font-size: 2.2em;
         display: inline-block;
-        margin-bottom: 30px;
+        width: 100px;
         margin-right: 40px;
+        margin-bottom: 30px;
+        font-size: 2.2em;
       }
 
       .third_desc {
-        line-height: 30px;
-        font-size: 2em;
         display: inline-block;
+        font-size: 2em;
+        line-height: 30px;
       }
     }
   }
@@ -269,9 +269,9 @@ export default {
     width: 90%;
 
     h2 {
-      // height: 00px;
       display: block;
       margin-left: 20px;
+      // height: 00px;
       // background-color: yellow;
 
       &::after {
@@ -281,26 +281,26 @@ export default {
     }
 
     .first {
-      // background-color: pink;
       margin-bottom: 20px;
+      // background-color: pink;
 
       .first_list {
-        // background-color: pink;
         top: -400px;
         left: 0;
+        // background-color: pink;
 
         &::before {
-          left: 4px;
           top: -30px;
+          left: 4px;
           height: 1550px;
         }
 
         .first_date {
+          margin-bottom: 20px;
           margin-left: 20px;
           font-size: 2em;
           // width: 90px;
           // background-color: pink;
-          margin-bottom: 20px;
         }
 
         .first_desc {
@@ -321,9 +321,9 @@ export default {
         left: 0;
 
         .second_date {
+          margin-bottom: 20px;
           margin-left: 20px;
           font-size: 2em;
-          margin-bottom: 20px;
         }
 
         .second_desc {
@@ -339,13 +339,13 @@ export default {
     .third {
 
       .third_list {
-        left: 0;
         top: -400px;
+        left: 0;
 
         .third_date {
-          font-size: 2em;
           margin-bottom: 20px;
           margin-left: 20px;
+          font-size: 2em;
         }
 
         .third_desc {
